81d519b856c90274109917a53df473ea5de46f36
erfan <erfankiani989@gmail.com>
2022-04-24 13:52:39 +0200

    compelete search and admin

--- pages/views.py
+++ pages/views.py
@@ -8,11 +8,25 @@ def home(request):
     teams=Team.objects.all()
     featured_cars=Car.objects.all().order_by('-created_date').filter(is_featured=True)
     all_cars=Car.objects.all().order_by('-created_date')
-    
+    # search_fields=Car.objects.values('model','city','year','body_style')
+    # print(search_fields)
+    model_search=Car.objects.values_list('model',flat=True).distinct()
+    city_search=Car.objects.values_list('city',flat=True).distinct()
+    year_search=Car.objects.values_list('year',flat=True).distinct()
+    body_style_search=Car.objects.values_list('body_style',flat=True).distinct()
+  
+    print(model_search)
     data={
         "teams":teams,
         "featured_cars":featured_cars,
-        "all_cars":all_cars
+        "all_cars":all_cars,
+        # "search_fields":search_fields
+        "model_search":model_search,
+        "city_search":city_search,
+        "year_search":year_search,
+        "body_style_search":body_style_search,
+    
+        
     }
     return render(request , 'pages/home.html',context=data)
 
